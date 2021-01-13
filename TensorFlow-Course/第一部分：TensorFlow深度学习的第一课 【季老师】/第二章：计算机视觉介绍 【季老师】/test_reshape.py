74b8cb4aa40c8d43ff111696feedd02ea097c430
lzx <zhuoxuan_li@foxmail.com>
2021-01-13 13:57:41 +0800

    numpy test

---
+++ TensorFlow-Course/第一部分：TensorFlow深度学习的第一课 【季老师】/第二章：计算机视觉介绍 【季老师】/test_reshape.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# a = np.zeros((28, 28), dtype=float)
+# print(a.shape)
+# c = a.reshape(1, 28, 28, 1)
+# print(c.shape)
+# # print(a.reshape(1, 28, 28, 1))
+# b = [1, 2, 3, 4]
+# print(type(a))
+np.random.seed(555)
+print(np.random.rand(10))
